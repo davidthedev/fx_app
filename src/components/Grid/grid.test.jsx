ca256de9dd9b8e341e47a897d7dde2220fea5fe8
David C <david.chubabriya@gmail.com>
2019-07-02 23:50:55 +0100

    Fix tests

--- src/components/Grid/grid.test.jsx
+++ src/components/Grid/grid.test.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import sinon from 'sinon';
-import Grid from '.';
-import Panel from '../Panel';
-import { generate } from '../utils/dataGenerator';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import sinon from "sinon";
+import Grid from ".";
+import Panel from "../Panel";
+import { generate } from "../utils/dataGenerator";
 
-describe('Grid', () => {
-  describe('should render', () => {
-    it('the component', () => {
+describe("Grid", () => {
+  describe("should render", () => {
+    it("the component", () => {
       const wrapper = shallow(<Grid />);
-      const shallowWrapper = wrapper.find('.c-grid');
+      const shallowWrapper = wrapper.find(".c-grid");
 
       expect(shallowWrapper.exists()).toBe(true);
     });
 
-    it('the component with 6 <Panel /> components', () => {
+    it("the component with 6 <Panel /> components", () => {
       const wrapper = mount(<Grid />);
       wrapper.setState({ data: generate() });
       const panels = wrapper.find(Panel);
@@ -24,12 +24,12 @@ describe('Grid', () => {
     });
   });
 
-  describe('should', () => {
-    it('call updateState every second', () => {
+  describe("should", () => {
+    it("call updateState every second", () => {
       const wrapper = shallow(<Grid />);
       const clock = sinon.useFakeTimers();
       const instance = wrapper.instance();
-      const updateStateStub = sinon.stub(instance, 'updateState');
+      const updateStateStub = sinon.stub(instance, "updateState");
 
       expect(instance.state.data.length).toEqual(0);
 
@@ -39,10 +39,12 @@ describe('Grid', () => {
       expect(updateStateStub.callCount).toEqual(2); // gets called twice in 2 seconds
     });
 
-    it('update the state on componentDidMount call', () => {
+    it("update the state on componentDidMount call", () => {
       const wrapper = shallow(<Grid />);
       const clock = sinon.useFakeTimers();
       const instance = wrapper.instance();
+
+      // console.log(instance.state);
 
       expect(instance.state.data.length).toEqual(0);
 
@@ -51,5 +53,5 @@ describe('Grid', () => {
 
       expect(instance.state.data.length).toEqual(6);
     });
-  })
+  });
 });
--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import Panel from '../Panel';
-import { generate } from '../utils/dataGenerator';
+import React, { Component } from "react";
+import Panel from "../Panel";
+import { generate } from "../utils/dataGenerator";
 
 export default class Grid extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: generate()
+      data: []
     };
 
     this.updateState = this.updateState.bind(this);
@@ -21,20 +21,19 @@ export default class Grid extends Component {
 
   buildPanels() {
     return this.state.data.map((row, index) => {
-      return <Panel
-              key={index}
-              currencyPair={row.pair}
-              sellPrice={row.sell}
-              buyPrice={row.buy} />;
+      return (
+        <Panel
+          key={index}
+          currencyPair={row.pair}
+          sellPrice={row.sell}
+          buyPrice={row.buy}
+        />
+      );
     });
   }
 
   render() {
-    return (
-      <div className="c-grid">
-        {this.buildPanels()}
-      </div>
-    )
+    return <div className="c-grid">{this.buildPanels()}</div>;
   }
 
   componentDidMount() {
